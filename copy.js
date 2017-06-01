9e0a4e8250d59df47dfcc1e378e71b8379f11aba
Mohamed GANA <mohamed.gana@amadeus.com>
2017-06-01 20:13:39 +0200

    Piping streams : taking the output of one stream and feeding it into the input of another

--- copy.js
+++ copy.js
@@ -46,6 +46,11 @@ fs.stat(file , (err, stat) => {
 
 	let read = fs.createReadStream(file)
 
+	let write = fs.createWriteStream('demo-copy.mp4')
+
+	//on peut greffer un 2eme stream d ecriture avec le meme stream de lecture
+	let write2 = fs.createWriteStream('demo-copy2.mp4')
+
 	//chunk est un morceau , ici de type buffer
 	//http://devdocs.io/node/stream#stream_class_stream_readable
 	read.on('data', (chunk) => {
@@ -54,8 +59,42 @@ fs.stat(file , (err, stat) => {
 		console.log('J ai lu ' + Math.round( (100*progress/total)) + "%" )
 	} )
 
-	read.on('end', () => {
-		console.log('J ai fini la lecture')
+
+	//j envoie les info que tu veux ecrire au writable stream
+	//en interne NodeJS va se charger de gere les cas ou on a des pauses, ...
+	read.pipe(write)
+	read.pipe(write2)
+
+	//on ecoute le finish
+	write.on('finish', ()=> {
+		console.log('Le fichier a bien ete copié')
 	})
 
 })
+
+
+/*
+
+--- Piping streams : taking the output of one stream and feeding it into the input of another ---
+
+
+Contrairement a la methode precedante , on lit le fichier au fur et a mesure 
+Ce qui veut dire que dans cette variable chunk , je n ai qu une partie du fichier
+donc je n est pas a stocker une grande partie d info pour pouvoir l utiliser plus tard
+
+l autre avantage c est que les flux peuvent etre greffés les un aux autres avec fs.createWriteStream
+Ce flux va pouvoir recevoir des info et les ecrire
+
+Les streams offrent la possibilité de gerer des cas d'engorgement 
+cad si je prend un disque dur qui est tres rapide en lecture mais tres lent en ecriture 
+--> le stream de lecture va etre plus rapide que celui de l ecriture
+donc le stream d ecriture ne va pas arriver a suivre
+donc il faut mettre en paus la lecture , continuer a ecrire je morceau puis reprendre la lecture
+
+il y a une methode tres pratique sur les streams qui est l utilisation du Pipe
+Le pipe permet de brancher 2 streams ensemble
+
+
+*** Cette methode va etre tres utile surtout lorsque on recoit des requetes assez volumineuse
+pour uploader des media par example
+*/
